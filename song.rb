753a4d22aaf53bcfb1f09fd9f0ee1070ef488153
Oluseun D Sowemimo <oluseun.sowemimo@gmail.com>
2017-03-01 11:33:33 +0000

    Add setup for postgres database connection

--- song.rb
+++ song.rb
@@ -4,6 +4,10 @@ configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
+configure :production do
+  DataMapper.setup(:default, 'postgres://daisi:12345@localhost/database')
+end
+
 class Song
   include DataMapper::Resource
   property :id, Serial
