01b59474fa7694c5c9c13a5defc90b502da448c8
Oluseun D Sowemimo <oluseun.sowemimo@gmail.com>
2017-03-01 13:01:11 +0000

    Add postgres database connection

--- main.rb
+++ main.rb
@@ -8,6 +8,14 @@ configure do
   set :password, 'sinatra'
 end
 
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 get '/login' do
   haml :login
 end
--- song.rb
+++ song.rb
@@ -1,13 +1,5 @@
 require 'data_mapper'
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
-
-configure :production do
-  DataMapper.setup(:default, 'postgres://daisi:12345@herokuapp.com/database')
-end
-
 class Song
   include DataMapper::Resource
   property :id, Serial
